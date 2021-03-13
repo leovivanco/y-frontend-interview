b859ef755dda01d380bc7c57588ff979c7776331
Leonardo Vivanco <leonardo.vivanco@wetek.com>
2021-03-13 19:11:03 +0000

    Add init config & update files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
+import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Welcome to the <strong>Random Fact Generator</strong> App!
-                </p>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to the <strong>Random Fact Generator</strong> App!
+        </p>
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App

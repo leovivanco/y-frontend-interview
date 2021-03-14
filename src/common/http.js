4d80a413ca7b0a64bf1c8c3956d7744334990210
Leonardo Vivanco <leonardo.vivanco@wetek.com>
2021-03-14 16:57:11 +0000

    Add limit year

--- src/common/http.js
+++ src/common/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE = 'http://numbersapi.com/random/year?json'
+const BASE = 'http://numbersapi.com/random/year?min=1000&max=2021&json'
 
 export const getRandomDate = async () => {
   return axios.get(BASE)

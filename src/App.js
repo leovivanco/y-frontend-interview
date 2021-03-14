796701310f661c255d44f053225ee67b8dd7caf3
Leonardo Vivanco <leonardo.vivanco@wetek.com>
2021-03-14 21:57:14 +0000

    Add Provider in app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import { useState } from 'react'
-import useRandomFacts from './hooks/useRandomFacts'
 import AppLayout from './components/ui/AppLayout'
-import { TimeLineContext } from './contexts/TimeLineContext'
+import { TimeLineProvider } from './contexts/TimeLineContext'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import TimeLinePage from './pages/TimeLinePage'
 
 const App = () => {
-  const { randomFacts, getNewFact, newFact } = useRandomFacts()
-  const [currentFact, setCurrentFact] = useState('')
-
   return (
-    <TimeLineContext.Provider
-      value={{
-        randomFacts,
-        getNewFact,
-        currentFact,
-        setCurrentFact,
-        newFact
-      }}
-    >
+    <TimeLineProvider>
       <AppLayout>
         <Router>
           <Switch>
             <Route path="/" exact component={HomePage} />
             <Route path="/timeline" exact component={TimeLinePage} />
           </Switch>
-          {/* <Col>{currentFact?.text}</Col>
-        <Col>
-          <TimeLine />
-        </Col> */}
         </Router>
       </AppLayout>
-    </TimeLineContext.Provider>
+    </TimeLineProvider>
   )
 }
 

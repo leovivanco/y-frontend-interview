421b2f6ae0851b26a8b4ab540bdcc6ae627df8d0
Leonardo Vivanco <leonardo.vivanco@wetek.com>
2021-03-14 20:09:59 +0000

    Add config router

--- src/App.js
+++ src/App.js
@@ -2,30 +2,37 @@ import { useState } from 'react'
 import useRandomFacts from './hooks/useRandomFacts'
 import AppLayout from './components/ui/AppLayout'
 import { TimeLineContext } from './contexts/TimeLineContext'
-import TimeLine from './components/TimeLine'
-import { Col } from './components/ui'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import TimeLinePage from './pages/TimeLinePage'
 
 const App = () => {
   const { randomFacts, getNewFact, newFact } = useRandomFacts()
   const [currentFact, setCurrentFact] = useState('')
 
   return (
-    <AppLayout>
-      <TimeLineContext.Provider
-        value={{
-          randomFacts,
-          getNewFact,
-          currentFact,
-          setCurrentFact,
-          newFact
-        }}
-      >
-        <Col>{currentFact?.text}</Col>
+    <TimeLineContext.Provider
+      value={{
+        randomFacts,
+        getNewFact,
+        currentFact,
+        setCurrentFact,
+        newFact
+      }}
+    >
+      <AppLayout>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/timeline" exact component={TimeLinePage} />
+          </Switch>
+          {/* <Col>{currentFact?.text}</Col>
         <Col>
           <TimeLine />
-        </Col>
-      </TimeLineContext.Provider>
-    </AppLayout>
+        </Col> */}
+        </Router>
+      </AppLayout>
+    </TimeLineContext.Provider>
   )
 }
 

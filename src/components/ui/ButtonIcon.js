d9a41bbee485fde9260a5f0d3c11662d4b405456
Leonardo Vivanco <leonardo.vivanco@wetek.com>
2021-03-14 21:55:49 +0000

    Add Button Icon delete

---
+++ src/components/ui/ButtonIcon.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const ButtonIcon = styled.button`
+  padding: 1rem;
+  background-color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+  color: #000;
+  text-decoration: none;
+  text-align: center;
+  border: transparent;
+  /* ${(props) =>
+    props.icon &&
+    `
+    ba
+   `} */
+`
+export default ButtonIcon
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,6 @@
 import Col from './Col'
 import AppLayout from './AppLayout'
 import Button from './Button'
+import ButtonIcon from './ButtonIcon'
 
-export { Col, AppLayout, Button }
+export { Col, AppLayout, Button, ButtonIcon }
